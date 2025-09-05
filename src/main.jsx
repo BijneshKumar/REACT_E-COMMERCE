4a47907dabacc365686bcadf13255e3b46df6ce3
bijnesh kumar <bijneshkumar10@gmail.com>
2025-09-05 18:15:19 +0530

    add basename in browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
 
   <StrictMode>
   <DataState>
-      <BrowserRouter>
+      <BrowserRouter basename='REACT_E-COMMERCE'>
       <App />
     </BrowserRouter>
   </DataState>
